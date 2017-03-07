abfca53f927db2cc0fd0ca45186f2bbd41093f0e
Tomasz Sułkowski <Tomasz Sukowski>
2017-03-07 19:52:33 +0100

    07/03/2017

--- publish.bat
+++ publish.bat
@@ -6,4 +6,13 @@ mkdir _site
 
 "C:\Program Files\7-Zip\7z.exe" a -r -y ..\CodeAndDrive.zip *
 
-call jekyll build & move ..\CodeAndDrive.zip _site/. & copy _site\* D:\git\soolek.github.io\. /Y & cd D:\git\soolek.github.io & git add . & git commit -m %date% & git push & cd D:\CodeAndDrive\
+call jekyll build
+
+move ..\CodeAndDrive.zip _site/.
+xcopy _site\* D:\git\soolek.github.io\. /E /C /H /R /Y
+
+cd D:\git\soolek.github.io
+git add .
+git commit -m %date%
+git push
+cd D:\CodeAndDrive\
