dfa14c30014333884f5d8d00a833779ce0b234d8
Tomasz Sułkowski <Tomasz Sukowski>
2017-03-07 19:34:39 +0100

    07/03/2017

--- publish.bat
+++ publish.bat
@@ -6,4 +6,4 @@ mkdir _site
 
 "C:\Program Files\7-Zip\7z.exe" a -r -y ..\CodeAndDrive.zip *
 
-call jekyll build & move ..\CodeAndDrive.zip _site/. & copy _site\* D:\git\soolek.github.io\. /Y & cd D:\git\soolek.github.io & git add . & git commit -m %date% & git push
+call jekyll build & move ..\CodeAndDrive.zip _site/. & copy _site\* D:\git\soolek.github.io\. /Y & cd D:\git\soolek.github.io & git add . & git commit -m %date% & git push & cd D:\CodeAndDrive\
